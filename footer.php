f5cefa5d9d5e0dbb4979af26f619cda794fa0026
isaacem178 <isaacmaldonado178@gmail.com>
2025-11-15 00:48:51 -0400

    themeWP

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div> <!-- #contenido -->
+
+    <footer class="site-footer" role="contentinfo">
+        desde footer
+    </footer>
+
+</div><!-- #page -->
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="pingback" href="<?php bloginfo('pingback_url') ?>">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="page"> 
+        <header id="masthead" class="site-header" role="banner">
+            <div class="container">
+                <a href="<?php echo esc_url(home_url('/')); ?>">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.svg">
+                </a>
+            </div>
+        </header>
+
+        <div id="contenido" class="container">
